809611c5513cb3c6f6e3f1f9a6511dcfb679e09b
NessDan <NessDan@users.noreply.github.com>
2023-01-16 15:06:33 -0500

    Move saving profiles to shared function

--- frontend.js
+++ frontend.js
@@ -364,38 +364,9 @@ document.querySelectorAll("input, select, option").forEach((inputEl) => {
 });
 
 saveConfigToDiskEl.addEventListener("click", (evt) => {
-  // https://stackoverflow.com/a/34156339/231730
-  function download(content, fileName, contentType) {
-    var a = document.createElement("a");
-    var file = new Blob([content], { type: contentType });
-    a.href = URL.createObjectURL(file);
-    a.download = fileName;
-    a.click();
-  }
-
   evt.preventDefault();
-
   const mappingsToDownload = mappingsToFullMappingStructure(mappings);
-
-  const fileName = prompt("Enter a file name to save the config to:");
-  if (fileName) {
-    download(
-      JSON.stringify(mappingsToDownload),
-      fileName + ".keyboardgg",
-      "application/json"
-    );
-  } else {
-    const date = new Date();
-    const fileNameDate =
-      [date.getMonth() + 1, date.getDate(), date.getFullYear()].join("-") +
-      " " +
-      [date.getHours(), date.getMinutes(), date.getSeconds()].join("-");
-    download(
-      JSON.stringify(mappingsToDownload),
-      fileNameDate.toLocaleString() + ".keyboardgg",
-      "application/json"
-    );
-  }
+  saveProfileToJSON(mappingsToDownload);
 });
 
 deleteConfigEl.addEventListener("click", (evt) => {
